cb6fc824b0235c3eb269a5199ed838b6b5d5b62e
Khitish Kumar Pradhan <khitishkumarpradhan55@gmail.com>
2025-02-26 23:24:39 +0530

    minor changes done

--- src/pages/admin/studentRelated/SeeComplains.js
+++ src/pages/admin/studentRelated/SeeComplains.js
@@ -52,7 +52,7 @@ const SeeComplains = () => {
         :
         <>
           {response ?
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px' ,marginRight:'10px'}}>
               No Complains Right Now
             </Box>
             :
